e22390d70e80880f3ae806810b27acc730915f28
Nyako <Nya.4@outlook.com>
2024-01-25 01:39:38 +0800

    好像能玩了

--- FogmoeGames/FogmoeGames/consumers.py
+++ FogmoeGames/FogmoeGames/consumers.py
@@ -1,4 +1,7 @@
 import json
+import threading
+import time
+import asyncio
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 from channels.generic.websocket import AsyncWebsocketConsumer
@@ -137,6 +140,9 @@ class ChatConsumer(WebsocketConsumer):
 
 ##------------------狼人杀---------------------------
 class WerewolfSagaConsumer(WebsocketConsumer):
+    wolfvote = [0,0,0,0,0,0]
+    witchvote=[0,0,0,0,0,0]
+    vote=[0,0,0,0,0,0]
     # websocket建立连接时执行方法
     def connect(self):
         print('websocket建立连接时执行方法')
@@ -217,18 +223,15 @@ class WerewolfSagaConsumer(WebsocketConsumer):
         player=WerewolfSagaPlayer.objects.get(player__id=user.id,werewolfsaga_id=self.roomId)
         roomUsers=room.players.all()
         roomPlayer=WerewolfSagaPlayer.objects.filter(werewolfsaga_id=self.roomId)
-            
         message = user.username+" : "+text_data_json['message']
         print(message)
         ##回合0
-        if room.round == 0:
+        if room.round==0 :
             if text_data_json['message'] == 'unready':
-                player.playerstatus = 0
-                player.save()
+               WerewolfSagaPlayer.objects.filter(id=player.id).update(playerstatus=0)
 
             if text_data_json['message'] == 'ready':
-                player.playerstatus = 1
-                player.save()
+                WerewolfSagaPlayer.objects.filter(id=player.id).update(playerstatus=1)
                 ##每有人准备时 判断房间中所有人是否准备
                 r=True
                 for p in roomPlayer:
@@ -236,91 +239,202 @@ class WerewolfSagaConsumer(WebsocketConsumer):
                 if r:
                     room.round = 1
                     room.save()
+                        
 
 
         ##第一阶段初始化
-        if room.round == 1:
+        if  room.round== 1:
             ##赋予编号
             i=0
             for p in roomPlayer:
                 i+=1
                 WerewolfSagaPlayer.objects.filter(id=p.id).update(playernumber=i)
-            #让所有玩家活着，并可以行动
-            WerewolfSagaPlayer.objects.filter(werewolfsaga=room).update(playerstatus=2,action=1)
+
             ##赋予角色
             if(WerewolfSagaPlayer.objects.filter(playernumber=1,werewolfsaga_id=self.roomId).exists()):
-                p=WerewolfSagaPlayer.objects.get(playernumber=1,werewolfsaga_id=self.roomId)
-                WerewolfSagaPlayer.objects.filter(id=p.id).update(role=1)
+                WerewolfSagaPlayer.objects.filter(playernumber=1,werewolfsaga_id=self.roomId).update(role=1)
             if(WerewolfSagaPlayer.objects.filter(playernumber=2,werewolfsaga_id=self.roomId).exists()):
-                p=WerewolfSagaPlayer.objects.get(playernumber=2,werewolfsaga_id=self.roomId)
-                WerewolfSagaPlayer.objects.filter(id=p.id).update(role=2)
+                WerewolfSagaPlayer.objects.filter(playernumber=2,werewolfsaga_id=self.roomId).update(role=2)
             if(WerewolfSagaPlayer.objects.filter(playernumber=3,werewolfsaga_id=self.roomId).exists()):
-                p=WerewolfSagaPlayer.objects.get(playernumber=3,werewolfsaga_id=self.roomId)
-                WerewolfSagaPlayer.objects.filter(id=p.id).update(role=3)
+                WerewolfSagaPlayer.objects.filter(playernumber=3,werewolfsaga_id=self.roomId).update(role=3)
             if(WerewolfSagaPlayer.objects.filter(playernumber=4,werewolfsaga_id=self.roomId).exists()):
-                p=WerewolfSagaPlayer.objects.get(playernumber=4,werewolfsaga_id=self.roomId)
-                WerewolfSagaPlayer.objects.filter(id=p.id).update(role=3)
+                WerewolfSagaPlayer.objects.filter(playernumber=4,werewolfsaga_id=self.roomId).update(role=3)
             if(WerewolfSagaPlayer.objects.filter(playernumber=5,werewolfsaga_id=self.roomId).exists()):
-                p=WerewolfSagaPlayer.objects.get(playernumber=5,werewolfsaga_id=self.roomId)
-                WerewolfSagaPlayer.objects.filter(id=p.id).update(role=3)
+                WerewolfSagaPlayer.objects.filter(playernumber=5,werewolfsaga_id=self.roomId).update(role=3)
             if(WerewolfSagaPlayer.objects.filter(playernumber=6,werewolfsaga_id=self.roomId).exists()):
-                p=WerewolfSagaPlayer.objects.get(playernumber=6,werewolfsaga_id=self.roomId)
-                WerewolfSagaPlayer.objects.filter(id=p.id).update(role=1)
-
+                WerewolfSagaPlayer.objects.filter(playernumber=6,werewolfsaga_id=self.roomId).update(role=1)
+            #让所有玩家活着，并可以行动
+            WerewolfSagaPlayer.objects.filter(werewolfsaga=room).update(playerstatus=2)
+            WerewolfSagaPlayer.objects.filter(werewolfsaga=room,role=1).update(action=1)
+            WerewolfSagaPlayer.objects.filter(werewolfsaga=room,role=2).update(action=1)
+            
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
+                {
+                    'type': 'chat_message',
+                    'message': '现在是晚上，请狼人和女巫鲨鱼人，60秒后夜晚结束。'
+                }
+            )
             WerewolfSaga.objects.filter(id=room.id).update(round=2)
+            ##进入round2 60秒后变成round3
+            
+            threading.Thread(target=lambda: [time.sleep(15), WerewolfSaga.objects.filter(id=room.id).update(round=3),self.receive(text_data)]).start()
+
+        ##第二阶段 游戏真正开始
+        if  room.round== 2:
+            match text_data_json['message']:
+                case 'killp1':
+                    print('这里进行投票计数1')
+                    if player.role == 1:
+                        self.wolfvote[0]+=1
+                        print('这里进行投票计数2',self.wolfvote)
+                    if player.role == 2:
+                        self.witchvote[0]+=1
+                        print('这里进行投票计数3',self.witchvote)
+                    WerewolfSagaPlayer.objects.filter(id=player.id).update(action=0)#杀死一个人后让用户不能再次杀人
+                case 'killp2':
+                    if player.role == 1:
+                        self.wolfvote[1]+=1
+                    if player.role == 2:
+                        self.witchvote[1]+=1
+                    WerewolfSagaPlayer.objects.filter(id=player.id).update(action=0)#杀死一个人后让用户不能再次杀人
+                case 'killp3':
+                    if player.role == 1:
+                        self.wolfvote[2]+=1
+                    if player.role == 2:
+                        self.witchvote[2]+=1
+                    WerewolfSagaPlayer.objects.filter(id=player.id).update(action=0)#杀死一个人后让用户不能再次杀人
+                case 'killp4':
+                    if player.role == 1:
+                        self.wolfvote[3]+=1
+                    if player.role == 2:
+                        self.witchvote[3]+=1
+                    WerewolfSagaPlayer.objects.filter(id=player.id).update(action=0)#杀死一个人后让用户不能再次杀人
+                case 'killp5':
+                    if player.role == 1:
+                        self.wolfvote[4]+=1
+                    if player.role == 2:
+                        self.witchvote[4]+=1
+                    WerewolfSagaPlayer.objects.filter(id=player.id).update(action=0)#杀死一个人后让用户不能再次杀人
+                case 'killp6':
+                    if player.role == 1:
+                        self.wolfvote[5]+=1
+                    if player.role == 2:
+                        self.witchvote[5]+=1
+                    WerewolfSagaPlayer.objects.filter(id=player.id).update(action=0)#杀死一个人后让用户不能再次杀人
+
+
+
+        #夜晚结算
+        if  room.round== 3:
+            print('夜晚结算中')
+            ##鲨了狼人和女巫投票的玩家
+            max_wolfvote = self.wolfvote.index(max(self.wolfvote))
+            max_witchvote = self.witchvote.index(max(self.witchvote))
+            print('狼人投票数据：',self.wolfvote)
+            print('狼人最大下标',max_wolfvote)
+            print('女巫投票数据：',self.witchvote)
+            print('女巫最大下标',max_witchvote)
+            message='白天到了， 现在可以自由发言，60秒后白天结束。'
+            if max(self.wolfvote)!=0:
+                WerewolfSagaPlayer.objects.filter(playernumber=max_wolfvote+1,werewolfsaga_id=self.roomId).update(playerstatus = 3)
+                message=f'狼鲨了{max_wolfvote+1}号玩家。\n'+message
+            if max(self.witchvote)!=0:
+                message=f'女巫鲨了{max_witchvote+1}号玩家。\n'+message
+                WerewolfSagaPlayer.objects.filter(playernumber=max_witchvote+1,werewolfsaga_id=self.roomId).update(playerstatus = 3)
 
             # 给玩家发送信息
             async_to_sync(self.channel_layer.group_send)(
                 self.room_group_name,
                 {
                     'type': 'chat_message',
-                    'message': '现在是晚上，请狼人和女巫鲨鱼人'
+                    'message': f'{message}'
                 }
             )
-            
+            #所有活着的人能动
+            WerewolfSagaPlayer.objects.filter(werewolfsaga=room,playerstatus=2).update(action=1)
+            #死了的请装死别动
+            WerewolfSagaPlayer.objects.filter(werewolfsaga=room,playerstatus=3).update(action=0)
+            #进入round4白天，60秒后进入夜晚
+            WerewolfSaga.objects.filter(id=room.id,).update(round=4)
+            threading.Thread(target=lambda: [time.sleep(15), WerewolfSaga.objects.filter(id=room.id).update(round=5),self.receive(text_data)]).start()
 
-        ##第二阶段 游戏真正开始
-        if room.round == 2:
+            ##白天
+        if  room.round== 4 :
+            # 发送消息到频道组，频道组调用chat_message方法
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
+                {
+                    'type': 'chat_message',
+                    'message': message
+                }
+            )
             match text_data_json['message']:
                 case 'killp1':
-                    p=WerewolfSagaPlayer.objects.get(playernumber=1,werewolfsaga_id=self.roomId)
-                    p.playerstatus = 3
-                    p.save()
+                    self.vote[0]+=1
                     WerewolfSagaPlayer.objects.filter(id=player.id).update(action=0)#杀死一个人后让用户不能再次杀人
                 case 'killp2':
-                    p=WerewolfSagaPlayer.objects.get(playernumber=2,werewolfsaga_id=self.roomId)
-                    p.playerstatus = 3
-                    p.save()
+                    self.vote[1]+=1
                     WerewolfSagaPlayer.objects.filter(id=player.id).update(action=0)#杀死一个人后让用户不能再次杀人
                 case 'killp3':
-                    p=WerewolfSagaPlayer.objects.get(playernumber=3,werewolfsaga_id=self.roomId)
-                    p.playerstatus = 3
-                    p.save()
+                    self.vote[2]+=1
                     WerewolfSagaPlayer.objects.filter(id=player.id).update(action=0)#杀死一个人后让用户不能再次杀人
                 case 'killp4':
-                    p=WerewolfSagaPlayer.objects.get(playernumber=4,werewolfsaga_id=self.roomId)
-                    p.playerstatus = 3
-                    p.save()
+                    self.vote[3]+=1
                     WerewolfSagaPlayer.objects.filter(id=player.id).update(action=0)#杀死一个人后让用户不能再次杀人
                 case 'killp5':
-                    p=WerewolfSagaPlayer.objects.get(playernumber=5,werewolfsaga_id=self.roomId)
-                    p.playerstatus = 3
-                    p.save()
+                    self.vote[4]+=1
                     WerewolfSagaPlayer.objects.filter(id=player.id).update(action=0)#杀死一个人后让用户不能再次杀人
                 case 'killp6':
-                    p=WerewolfSagaPlayer.objects.get(playernumber=6,werewolfsaga_id=self.roomId)
-                    p.playerstatus = 3
-                    p.save()
+                    self.vote[5]+=1
                     WerewolfSagaPlayer.objects.filter(id=player.id).update(action=0)#杀死一个人后让用户不能再次杀人
-                    
-        # 发送消息到频道组，频道组调用chat_message方法
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message
-            }
-        )
+
+
+            ##白天结算，进入夜晚
+        if  room.round== 5 :
+            print('投票数据：',self.vote)
+            max_vote = self.vote.index(max(self.vote))
+            if max(self.vote)!=0:
+                WerewolfSagaPlayer.objects.filter(playernumber=max_vote+1,werewolfsaga_id=self.roomId).update(playerstatus = 3)
+                role='角色'
+                match WerewolfSagaPlayer.objects.get(playernumber=max_vote+1,werewolfsaga_id=self.roomId).role:
+                    case 0:
+                        role='鹿人'
+                    case 1:
+                        role='狼人'
+                    case 2:
+                        role='女巫'
+                    case 3:
+                        role='村民'
+                async_to_sync(self.channel_layer.group_send)(
+                    self.room_group_name,
+                    {
+                        'type': 'chat_message',
+                        'message': f' {max_vote+1}号玩家被大家鲨了，死者是一个{role}。\n夜晚降临，还活着的狼人女巫可以继续吃小孩，60秒后夜晚结束。'
+                    }
+                )
+            else:
+                async_to_sync(self.channel_layer.group_send)(
+                    self.room_group_name,
+                    {
+                        'type': 'chat_message',
+                        'message': f'白天过去了然而无事发生。\n夜晚降临，还活着的狼人女巫可以继续吃小孩，60秒后夜晚结束。'
+                    }
+                )
+
+            
+            #狼和女巫能动
+            WerewolfSagaPlayer.objects.filter(werewolfsaga=room,role=1,playerstatus=2).update(action=1)
+            WerewolfSagaPlayer.objects.filter(werewolfsaga=room,role=2,playerstatus=2).update(action=1)
+            #死了的别动
+            WerewolfSagaPlayer.objects.filter(werewolfsaga=room,playerstatus=3).update(action=0)
+            WerewolfSaga.objects.filter(id=room.id).update(round=2)
+            #进入round3
+            threading.Thread(target=lambda: [time.sleep(15), WerewolfSaga.objects.filter(id=room.id).update(round=3),self.receive(text_data)]).start()
+                
+
+
+
 
         # 同步玩家列表
         async_to_sync(self.channel_layer.group_send)(
@@ -391,5 +505,4 @@ class WerewolfSagaConsumer(WebsocketConsumer):
             'playeraction': f'{str(playeraction)}',
             'lifelist': f'{str(lifelist)}'
         }))
-
-
+    
